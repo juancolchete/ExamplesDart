59832275f5044a35c019c5d1326f7638aba232c1
juanudk <juanudkp@gmail.com>
2021-06-28 23:30:32 -0300

    Sum two numbers and print

---
+++ sumTwoNumbers.dart
@@ -0,0 +1,8 @@
+main(){
+    print(sumTwoNumbers(firstNumber: 6,secondNumber: 2));
+}
+
+int sumTwoNumbers({required int firstNumber, required int secondNumber}){
+    int sum = firstNumber + secondNumber;
+    return sum;
+}
